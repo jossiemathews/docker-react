197d15f8ac53e655abad307a2fab846a97d5d4a2
Jossie <jossiemathews@gmail.com>
2019-06-07 11:09:33 +0530

    change for the sake of it

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> AAAhaaa Haa.
+          Edit <code>src/App.js</code> AAAhaaa Haa. WOW
         </p>
         <a
           className="App-link"
